254f4b25babe3b0e95fc5d2930bc4bd198ce7ec1
6oogle <32604831+6oogle@users.noreply.github.com>
2018-07-15 22:23:28 +0300

    Ещё слушатели пакетов

--- src/pro/delfik/lmao/core/connection/PacketListener.java
+++ src/pro/delfik/lmao/core/connection/PacketListener.java
@@ -3,7 +3,10 @@ package pro.delfik.lmao.core.connection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import pro.delfik.lmao.core.OnlineHandler;
+import pro.delfik.lmao.core.Person;
 import pro.delfik.net.Packet;
+import pro.delfik.net.packet.PacketAuth;
+import pro.delfik.net.packet.PacketPex;
 import pro.delfik.net.packet.PacketUser;
 
 public class PacketListener implements Listener {
@@ -13,6 +16,11 @@ public class PacketListener implements Listener {
 		if(packet instanceof PacketUser){
 			PacketUser user = (PacketUser) packet;
 			OnlineHandler.waitingPackets.add(user.getNick().toLowerCase(), user);
+		}else if(packet instanceof PacketAuth){
+			Person.get(((PacketAuth) packet).getNick()).auth();
+		}else if(packet instanceof PacketPex){
+			PacketPex pex = (PacketPex)packet;
+			Person.get(pex.getNick()).setRank(pex.getRank());
 		}
 	}
 }
