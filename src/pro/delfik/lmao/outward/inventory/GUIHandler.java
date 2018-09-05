3325f195825ec197e0832cb49438ccf8d9699c53
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-09-05 19:55:59 +0300

    Проверка на пустой слот

--- src/pro/delfik/lmao/outward/inventory/GUIHandler.java
+++ src/pro/delfik/lmao/outward/inventory/GUIHandler.java
@@ -1,9 +1,13 @@
 package pro.delfik.lmao.outward.inventory;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class GUIHandler implements Listener {
 
@@ -13,6 +17,8 @@ public class GUIHandler implements Listener {
 		GUI gui = GUI.get(e.getClickedInventory());
 		if (gui == null) return;
 		e.setCancelled(true);
+		ItemStack i = e.getCurrentItem();
+		if (i == null || i.getType() == Material.AIR) return;
 		gui.click(e);
 	}
 
@@ -20,6 +26,15 @@ public class GUIHandler implements Listener {
 	public void onClose(InventoryCloseEvent e) {
 		GUI gui = GUI.get(e.getInventory());
 		if (gui != null && gui.isTemporary()) gui.remove();
+	}
+
+	@EventHandler
+	public void onInteract(PlayerInteractEvent e) {
+		if (!(e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() != Action.RIGHT_CLICK_AIR)) return;
+		GUI gui = GUI.assigned.get(e.getMaterial());
+		if (gui == null) return;
+		e.setCancelled(true);
+		e.getPlayer().openInventory(gui.inv());
 	}
 
 }
